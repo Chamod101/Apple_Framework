f344e809db2082c55b098063416b806e112e5640
chamod101 <chamoddperera99@gmail.com>
2024-02-12 06:18:09 -0800

    Initial Commit

---
+++ Apple_Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple_Framework
+//
+//  Created by Chamod Dilushanka on 2024-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
---
+++ Apple_Framework/FrameworkGridView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Apple_Framework
+//
+//  Created by Chamod Dilushanka on 2024-02-11.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns : [GridItem] = [GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible())]
+    
+    var body: some View {
+        
+        NavigationView{
+            
+            ScrollView{
+                LazyVGrid(columns:columns){
+                    ForEach(MockData.frameworks, id: \.id){framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            
+            .navigationTitle("Apple Framework")
+        }
+        
+        
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
+
+struct FrameworkTitleView: View{
+    
+    let framework : Framework
+    
+    var body: some View{
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90,height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+        }.padding()
+    }
+}
+
+
